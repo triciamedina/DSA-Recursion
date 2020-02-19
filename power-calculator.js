17c6edd6a5b46d301ff843c6a6f43e351221ac95
triciamedina <tricia.medina@gmail.com>
2020-02-18 20:57:43 -0800

    working powerCalculator algorithm

---
+++ power-calculator.js
@@ -0,0 +1,23 @@
+const powerCalculator = function(base, exponent) {
+    if (exponent < 0) {
+        return 'exponent should be >= 0';
+    };
+
+    if (exponent === 0) {
+        return 1
+    };
+
+    if (exponent > 0) {
+        // Base case
+        if (exponent === 1) {
+            return base
+        };
+        
+        // General case
+        return base * powerCalculator(base, --exponent);
+    }
+}
+
+let base = 3;
+let exponent = 2;
+console.log(powerCalculator(base, exponent));

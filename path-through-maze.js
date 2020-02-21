af6ab7ba2c7f869b0cf00fad22b007b0b69f19f2
triciamedina <tricia.medina@gmail.com>
2020-02-21 12:36:05 -0800

    working pathThroughMaze function

---
+++ path-through-maze.js
@@ -0,0 +1,38 @@
+const pathThroughMaze = function(maze, row, col) {
+    // Base case
+    if (maze[row][col] === 'e') {
+        return ''
+    }
+
+    // General case
+    if (maze[row][col] === ' ') {
+        if ((maze[row][col + 1] === ' ') || (maze[row][col + 1] === 'e')) {
+            return `R ${pathThroughMaze(maze, row, (col + 1))}`
+        } else if ((maze[row + 1][col] === ' ' )|| (maze[row + 1][col] === 'e')) {
+            return `D ${pathThroughMaze(maze, (row + 1), col)}`
+        } else if ((maze[row][col - 1] === ' ' )|| (maze[row][col - 1] === 'e')) {
+            return `L ${pathThroughMaze(maze, row, (col - 1))}`
+        } else {
+            return `U ${pathThroughMaze(maze, (row - 1), col)}`
+        }
+    }
+}
+
+let mySmallMaze = [
+    [' ', ' ', ' '],
+    [' ', '*', ' '],
+    [' ', ' ', 'e']
+];
+
+let bigMaze = [
+    [' ', ' ', ' ', '*', ' ', ' ', ' '],
+    ['*', '*', ' ', '*', ' ', '*', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', '*', '*', '*', '*', '*', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', 'e']
+];
+
+let row = 0;
+let col = 0;
+
+console.log(pathThroughMaze(bigMaze, row, col));
